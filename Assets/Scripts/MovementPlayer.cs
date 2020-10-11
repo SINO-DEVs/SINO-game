ae02f5eb2c2d936eb8bd044ee901df22eebb8e61
Samuel de Babo Martins <samuel.debabomartins@student.supsi.ch>
2020-10-11 20:36:00 +0200

    orbit camera

---
+++ Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    public float moveSpeed = 6.0f;
+    public float gravity = -9.8f;
+    private CharacterController _charController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+        float horInput = -Input.GetAxis("Horizontal");
+        float vertInput = -Input.GetAxis("Vertical");
+
+        if (horInput != 0 || vertInput != 0)
+        {
+            movement.x = horInput * moveSpeed;
+            movement.z = vertInput * moveSpeed;
+            
+            Vector3.ClampMagnitude(movement, moveSpeed);
+            //…
+        }
+        movement *= Time.deltaTime;
+        _charController.Move(movement);
+    }
+}
---
+++ Assets/Scripts/OrbitCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    public float rotSpeed = 1.5f;
+    private float _rotY;
+    private float _rotX;
+    private Vector3 _offset;
+    void Start()
+    {
+        _rotY = transform.eulerAngles.y;
+        _rotX = transform.eulerAngles.x;
+        _offset = target.position - transform.position;
+    }
+    void LateUpdate()
+    {
+        _rotY += Input.GetAxis("Mouse X") * rotSpeed * 3;
+        _rotX += Input.GetAxis("Mouse Y") * rotSpeed * -1;
+        Quaternion rotation = Quaternion.Euler(_rotX, 0, 0);
+        transform.position = target.position - (rotation * _offset);
+        transform.LookAt(target);
+    }
+}
