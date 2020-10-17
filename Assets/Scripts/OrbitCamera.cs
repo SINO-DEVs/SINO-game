9be03405ddf80ea36fd097bfe89ae3c1c6eb5304
Samuel Martins <tech1@smartins.ch>
2020-10-17 18:29:13 +0200

    Movement player with animations (#6)
    * orbit camera
    
    * fixed orbit camera
    
    * moved imported assets
    
    * animations and movement complete

---
+++ Assets/Scripts/OrbitCamera.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    public float rotXSpeed = 3.0f;
+    public float rotYSpeed = 1.5f;
+    public float maxYAngle = 20;
+    public float minYAngle = -20;
+
+    private float _rotY;
+    private float _rotX;
+    private Vector3 _offset;
+
+
+    void Start()
+    {
+        _rotY = transform.eulerAngles.y;
+        _rotX = transform.eulerAngles.x;
+        _offset = target.position - transform.position;
+    }
+
+    void LateUpdate()
+    {
+        _rotY += Input.GetAxis("Mouse X") * rotXSpeed;
+        _rotX -= Input.GetAxis("Mouse Y") * rotYSpeed;
+
+        if(_rotX > maxYAngle || _rotX < minYAngle) {
+            _rotX = _rotX > 0 ? maxYAngle : minYAngle;
+        }
+
+        Quaternion rotation = Quaternion.Euler(_rotX, _rotY, 0);
+        transform.position = target.position - (rotation * _offset);
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/RelativeMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelativeMovement : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    public float moveSpeed = 6.0f;
+    public float speedUpMultiplier = 2.0f;
+
+    private CharacterController _charController;
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool speedUp = Input.GetKey(KeyCode.LeftShift);
+        _animator.SetBool("isRunning", speedUp);
+
+        Vector3 movement = Vector3.zero;
+        float horInput = Input.GetAxis("Horizontal");
+        float vertInput = Input.GetAxis("Vertical");
+
+        bool idle = horInput == 0 && vertInput == 0;
+        _animator.SetBool("idle", idle);
+
+
+        if (!idle)
+        {
+            movement.x = horInput;
+            movement.z = vertInput;
+
+            Quaternion tmp = target.rotation;
+            target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+            movement = target.TransformDirection(movement);
+            target.rotation = tmp;
+            transform.rotation = Quaternion.LookRotation(movement);
+            Vector3.ClampMagnitude(movement, moveSpeed * (speedUp ? speedUpMultiplier : 1.0f));
+            GetComponent<CharacterController>().Move(movement * Time.deltaTime * moveSpeed * (speedUp ? speedUpMultiplier : 1.0f));
+        }
+    }
+}
