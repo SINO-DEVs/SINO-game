dc1b9ed6b60caa87ce0aa75a7b7ad2663c839f03
Samuel Martins <tech1@smartins.ch>
2020-10-24 18:51:38 +0200

    Implemented enemyVision and animation kill

---
+++ Assets/Scripts/EnemyVision.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVision : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    public float angleVision = 25;
+    public float distVision = 8;
+
+    private Animator _animator;
+    private bool targetKilled = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = Vector3.Angle(target.position - transform.position, transform.forward);
+        float dist = Vector3.Distance(target.position, transform.position);
+
+        //raycast to check obstacles
+        Ray ray = new Ray(transform.position, target.position - transform.position + Vector3.up * 0.5f);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        
+        if (angle < angleVision && dist < distVision && hit.collider.CompareTag("Player"))
+        {
+            //retrive script palyer
+            RelativeMovement targetScript = hit.transform.gameObject.GetComponent<RelativeMovement>();
+
+            //rotation guard in y
+            Quaternion rot = Quaternion.LookRotation(target.position - transform.position);
+            rot.x = 0;
+            rot.z = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, rot, 0.01f);
+
+            //animations
+            targetScript.reactToGuard(transform);
+            if (!targetKilled) //avoid repeating animation
+                _animator.SetBool("foundTarget", true);
+            StartCoroutine(AfterAnimation());
+        }   
+    }
+
+    private IEnumerator AfterAnimation()
+    {
+        yield return new WaitForSeconds(0.5f);
+        targetKilled = true;
+        _animator.SetBool("foundTarget", false);
+    }
+
+}
--- Assets/Scripts/RelativeMovement.cs
+++ Assets/Scripts/RelativeMovement.cs
@@ -11,6 +11,7 @@ public class RelativeMovement : MonoBehaviour
 
     private CharacterController _charController;
     private Animator _animator;
+    private bool isDead = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,9 @@ public class RelativeMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isDead)
+            return;
+
         bool speedUp = Input.GetKey(KeyCode.LeftShift);
         _animator.SetBool("isRunning", speedUp);
 
@@ -32,20 +36,50 @@ public class RelativeMovement : MonoBehaviour
         bool idle = horInput == 0 && vertInput == 0;
         _animator.SetBool("idle", idle);
 
-
         if (!idle)
         {
             movement.x = horInput;
             movement.z = vertInput;
 
+            //create movement vector from camera perspective
             Quaternion tmp = target.rotation;
             target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
             movement = target.TransformDirection(movement);
             target.rotation = tmp;
+
+            //rotation payer
             Quaternion to = Quaternion.LookRotation(movement);
             transform.rotation = Quaternion.Slerp(transform.rotation, to, 0.1f);
+
+            //movement palyer
             Vector3.ClampMagnitude(movement, moveSpeed * (speedUp ? speedUpMultiplier : 1.0f));
             _charController.Move(movement * Time.deltaTime * moveSpeed * (speedUp ? speedUpMultiplier : 1.0f));
         }
     }
+
+    public void reactToGuard(Transform guard)
+    {
+        //set parameters for animations
+        _animator.SetBool("idle", true);
+        _animator.SetBool("isRunning", false);
+
+        // disable movement player
+        isDead = true;
+
+        //rotation in y in the direction of the guard
+        Quaternion rot = Quaternion.LookRotation(guard.position - transform.position);
+        rot.x = 0;
+        rot.z = 0;
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, 0.01f);
+
+        //start animation after few moments
+        StartCoroutine(Die());
+    }
+
+    private IEnumerator Die()
+    {
+        yield return new WaitForSeconds(1.0f);
+        _animator.SetBool("isDead", isDead);
+    }
+
 }
