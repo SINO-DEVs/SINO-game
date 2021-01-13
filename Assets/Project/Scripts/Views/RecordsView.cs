9447f7b218fb2b3ca887c35446fb8dae204b41c4
Samuel Martins <tech1@smartins.ch>
2021-01-13 23:44:15 +0100

    record view

---
+++ Assets/Project/Scripts/Views/RecordsView.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class RecordsView : MonoBehaviour
+{
+    private Text scores;
+
+    private readonly int maxCharPoints = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scores = GetComponent<Text>();
+        scores.text = String.Format("Level 1: {0,10:D" + maxCharPoints + "}\n" +
+                                    "Level 2: {1,10:D" + maxCharPoints + "}\n" +
+                                    "Level 3: {2,10:D" + maxCharPoints + "}",
+                                    ScoreRepositoryManager.Instance.loadBestScoreFor("Level01"),
+                                    ScoreRepositoryManager.Instance.loadBestScoreFor("Level02"),
+                                    ScoreRepositoryManager.Instance.loadBestScoreFor("Level03"));
+    }
+
+    
+}
--- Assets/Project/Scripts/Views/ScoreView.cs
+++ Assets/Project/Scripts/Views/ScoreView.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ScoreView : MonoBehaviour
@@ -10,17 +11,21 @@ public class ScoreView : MonoBehaviour
 
     private readonly int maxCharPoints = 10;
 
+    private int record;
+
     // Start is called before the first frame update
     void Start()
     {
         scoreText = GetComponent<Text>();
         Messenger<int>.AddListener(GameEvent.SCORE_CHANGED, OnScoreChanged);
         OnScoreChanged(ScoreManager.Instance.Score);
+        record = ScoreRepositoryManager.Instance.loadBestScoreFor(SceneManager.GetActiveScene().name);
     }
 
     private void OnScoreChanged(int score)
     {
-        scoreText.text = String.Format("{0,10:D" + maxCharPoints + "}", score);
+        scoreText.text = String.Format("Record: {0,10:D" + maxCharPoints + "}\n" +
+                                       "Points : {1,10:D" + maxCharPoints + "}", record, score);
     }
 
     void OnDestroy()
