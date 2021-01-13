c1392dd5a2e6587e5c8ef4a5d1d50b46a14ea7be
Francesco Moro <francesco.moro@student.supsi.ch>
2021-01-13 21:42:13 +0100

    Added try again button in game over screen

---
+++ Assets/Project/Scripts/Button/RetryScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RetryScript : MonoBehaviour
+{
+    public Button retry;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Find the GameObject named Best in the scene
+        GameObject retryGameObject = GameObject.Find("RetryButton");
+        retry = retryGameObject.GetComponent<Button>();
+        retry.onClick.AddListener(Levels.Instance.retry);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void goToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
